4fc9d6b5b53d9695e12a7027660e4a7e965102e6
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2020-03-10 14:30:33 -0500

    [+] App > styled: Container, Heading

--- src/App.js
+++ src/App.js
@@ -1,8 +1,43 @@
 import React from 'react';
+import styled from '@emotion/styled';
+
+const 
+  Heading = styled .h1 `
+    color: #000;
+    font-size: 2.5rem;
+    font-weight: 700;
+    margin: 6rem 0 3rem 0;
+    text-align: left;
+
+    &::after {
+      background: #66A2FE;
+      display: block;
+      content: '';
+      height: 6px;
+      width: 100px;
+    }
+  `,
+  Container = styled .div `
+    margin: 0 auto;
+    max-width: 900px;
+    
+    @media ( min-width: 992px ) {
+      display: grid;
+      grid-template-columns: repeat( 2, 1fr );
+      column-gap: 2rem;
+    }
+  `;
 
 function App() {
   return (
-    <h1>CriptoCurrencies</h1>
+    <Container>
+      <div>
+        <Heading>Consulta Criptos</Heading>
+      </div>
+      <div>
+        <Heading>Valor Cotización</Heading>
+      </div>
+    </Container>
   );
 }
 
