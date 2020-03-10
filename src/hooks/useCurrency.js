4dcd7017307b187f33531efb1855754212294a97
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2020-03-10 15:40:21 -0500

    [+] useCurrency: Get value option select currency

--- src/hooks/useCurrency.js
+++ src/hooks/useCurrency.js
@@ -8,8 +8,11 @@ const useCurrency = ( label, initialState, options ) => {
     /** Interface que se mostrará (No siempre se muestra) */
     const SelectCurrency = () => (          // Return Implicito
         <Fragment>
-            <label>Moneda</label>
-            <select>
+            <label>{ label }</label>
+            <select
+                onChange={ event => setState( event .target .value ) }      /** Actualiza el State */
+                value={ state }                                             /** Asigna Valor al Select */
+            >
                 <option value="">Seleccione...</option>
                 { options .map( option => (     // Return implicito
                     <option 
