597890f57fc5bbcf54ea1174d52042b539748b21
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2020-03-10 16:11:12 -0500

    [+] useCriptoCurrency: Add style Components

--- src/hooks/useCriptoCurrency.js
+++ src/hooks/useCriptoCurrency.js
@@ -1,4 +1,24 @@
 import React, { Fragment } from 'react';
+import styled from '@emotion/styled';
+
+const 
+    Label = styled .label `
+        color: #326AC0;
+        display: block;
+        font-size: 2rem;
+        font-weight: bold;
+        margin: 1rem 0;
+    `,
+    Select = styled .select `
+        background-color: lightcyan;
+        border: 1px solid #326AC0;
+        border-radius: 10px;
+        display: block;
+        font-size: 1.2rem;
+        padding: .5rem;
+        width: 100%;
+        -webkit-appearance: none;
+    `;
 
 /** Hook */
 const useCriptoCurrency = () => {
@@ -6,10 +26,10 @@ const useCriptoCurrency = () => {
     /** Interface que se mostrará (No siempre se muestra una) */
     const SelectCriptoCurrency = () => (          // Return Implicito
         <Fragment>
-            <label>Elije tu cripto</label>
-            <select>
+            <Label>Elije tu cripto</Label>
+            <Select>
                 <option value="BTC">Bitcoin</option>
-            </select>
+            </Select>
         </Fragment>
     );
 
